8288695e78032690404483251e9d57610460c283
mrstevengz <ssequeira@uamv.edu.ni>
2024-04-30 11:14:11 -0600

    Ejercicio 3

---
+++ sesion3004/ejercicioFor3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAX 100
+
+int vector[MAX];
+int pos;
+
+void agregar(int num);
+void mostrar();
+
+int main(int argc, char const *argv[])
+{
+    int cant;
+    int num;
+    cout << "Ingrese la cantidad de numeros a ingresar: ";
+    cin >> cant;
+    for (int i = 0; i < cant; i++)
+    {
+        cout << "Dime un #: ";
+        cin >> num;
+        agregar(num);
+    }
+    cout << "Mostrar numeros \n";
+    mostrar();
+
+    return 0;
+}
+
+void agregar(int num){
+    vector[pos] = num;
+    pos++;
+}
+void mostrar(){
+    for (int i = 0; i < pos; i++)
+    {
+        cout << vector[i] << endl;
+    }
+}
