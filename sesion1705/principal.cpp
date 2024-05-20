a10983abc4e2483c9978ca06cf2072956e400e38
mrstevengz <ssequeira@uamv.edu.ni>
2024-05-19 21:52:34 -0600

    Ejercicios del 17 de mayo

---
+++ sesion1705/principal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "functions.cpp"
+
+/*Realiza un programa que permita almacenar x cantidad de
+edades, mostrar todas las edades, la edad mayor, la edad menor
+y el promedio de edades.*/
+
+using namespace std;
+
+int main()
+{
+    start();
+    return 0;
+}
