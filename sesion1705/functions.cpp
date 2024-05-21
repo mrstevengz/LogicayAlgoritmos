f9370a368b4c313e2b4c8f8a230afcdbee1941f3
mrstevengz <ssequeira@uamv.edu.ni>
2024-05-21 10:06:20 -0600

    Ejercicios del 17 de mayo

--- sesion1705/functions.cpp
+++ sesion1705/functions.cpp
@@ -3,14 +3,17 @@
 
 using namespace std;
 
-/*prototipo de funciones*/
+/*Prototipo de funciones*/
 void start();
 void menu();
 void addAge();
+void showAges();
+void showOldest();
+void showYounguest();
+void showAverage();
 
 /*Funciones*/
-void menu()
-{
+void menu(){
     system("cls||clear");
     cout << "1. Ingresar edades" << endl;
     cout << "2. Mostrar edades" << endl;
@@ -21,6 +24,7 @@ void menu()
 }
 
 /*Agregar edades*/
+
 void addAge()
 {
     system("cls||clear");
@@ -30,23 +34,11 @@ void addAge()
     system("pause");
 }
 
-/*Mostrar edades*/
-
-void showAge(){
-    system("cls||clear");
-    for (int i = 0; i < pos; i++)
-    {
-        cout << ages[i] << endl;
-    }
-    system("pause");
-
-}
-
 void start()
 {
     int option = 0;
 
-    while (option != 6)
+    while(option != 6)
     {
         menu();
         cout << "Dime una opcion: ";
@@ -57,13 +49,75 @@ void start()
             addAge();
             break;
         case 2:
-            showAge();
+            showAges();
+            break;
+        case 3:
+            showOldest();
+            break;
+        case 4:
+            showYounguest();
+            break;
+        case 5:
+            showAverage();
             break;
         case 6:
             cout << "Adios" << endl;
             break;
-        default:
+        default: 
             break;
         }
     }
+}
+
+void showAges()
+{
+    system("cls||clear");
+    cout << "Edades: " << endl;
+    for(int i = 0; i < pos; i++)
+    {
+        cout << ages[i] << endl;
+    }
+    system("pause");
+}
+
+void showOldest()
+{
+    system("cls||clear");
+    int maxAge = ages[0]; // Inicializar con el primer elemento
+
+    for(int i = 1; i < pos; i++) {
+        if(ages[i] > maxAge) {
+            maxAge = ages[i]; // Actualizar maxAge si encontramos una edad mayor
+        }
+    }
+
+    cout << "La edad mayor es: " << maxAge << endl;
+    system("pause");
+}
+
+void showYounguest()
+{
+    system("cls||clear");
+    int minAge = ages[0]; // Inicializar con el primer elemento
+
+    for(int i = 1; i < pos; i++) {
+        if(ages[i] < minAge) {
+            minAge = ages[i]; // Actualizar minAge si encontramos una edad menor
+        }
+    }
+
+    cout << "La edad menor es: " << minAge << endl;
+    system("pause");
+}
+
+void showAverage()
+{
+    system("cls||clear");
+    int sum = 0;
+    for(int i = 0; i < pos; i++)
+    {
+        sum += ages[i];
+    }
+    cout << "El promedio de edades es: " << sum / pos << endl;
+    system("pause");
 }
